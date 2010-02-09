2f824430731a642cc30e1e8e87297aaa4e30a4af
Joe Wölfel <joe@talkhouse.com>
2010-02-08 19:16:49 -0500

    Clean up the mel_filter java wrapper a bit.
    A minor amount of redudancy was removed.

--- lib/java_impl/java_filter.rb
+++ lib/java_impl/java_filter.rb
@@ -4,5 +4,12 @@ module Noyes
       java_matrix = @filter.apply data.to_java Java::double[]
       java_matrix.map {|java_array|java_array.to_a}
     end
+    def self.ensure_jarray array
+      if array.respond_to? :each
+        array.to_java(Java::double[]).to_a
+      else
+	array
+      end
+    end
   end
 end
--- lib/java_impl/mel_filter.rb
+++ lib/java_impl/mel_filter.rb
@@ -1,35 +1,27 @@
+require 'java_filter'
 module Noyes
   class MelFilter
+    include JavaFilter
     def initialize srate, nfft, nfilt, lowerf, upperf
-      @jmf = Java::talkhouse.MelFilter.new srate, nfft, nfilt, lowerf, upperf
-    end
-    def << power_spectrum
-      x = @jmf.apply power_spectrum.to_java Java::double[]
-      x.map {|a|a.to_a}
+      @filter = Java::talkhouse.MelFilter.new srate, nfft, nfilt, lowerf, upperf
     end
     def self.make_bank_parameters srate, nfft, nfilt, lowerf, upperf
-      x = Java::talkhouse.MelFilter.make_bank_parameters srate, nfft, nfilt, lowerf, upperf
-      x.map {|a|a.to_a}
+      parameters = Java::talkhouse.MelFilter.make_bank_parameters srate, nfft,
+                                                        nfilt, lowerf, upperf
+      parameters.map {|array|array.to_a}
     end
     def self.make_filter left, center, right, init_freq, delta
-      x = Java::talkhouse.MelFilter.make_filter left, center, right, init_freq, delta
-      x = x.to_a
-      index = x.shift
-      [index, x]
+      filters = Java::talkhouse.MelFilter.make_filter left, center, right,
+                                                      init_freq, delta
+      filters = filters.to_a
+      indefilters = filters.shift
+      [indefilters, filters]
     end
     def self.to_mel f
-      if f.respond_to? :each
-        Java::talkhouse.MelFilter.mel f.to_java(Java::double[]).to_a
-      else
-        x = Java::talkhouse.MelFilter.mel f
-      end
+      x = Java::talkhouse.MelFilter.mel JavaFilter.ensure_jarray f
     end
     def self.to_linear mel
-      if mel.respond_to? :each
-        Java::talkhouse.MelFilter.melinv mel.to_java(Java::double[]).to_a
-      else
-        Java::talkhouse.MelFilter.melinv mel
-      end
+      Java::talkhouse.MelFilter.melinv JavaFilter.ensure_jarray mel
     end
   end
 end
