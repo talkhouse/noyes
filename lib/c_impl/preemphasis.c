cce5b15e8d441d58cd5808048d44474ba9f29925
Joe Wölfel <joe@talkhouse.com>
2010-05-04 13:22:56 -0400

    Variable argument initializer with default of 0.97 for preemphasis.c

--- lib/c_impl/preemphasis.c
+++ lib/c_impl/preemphasis.c
@@ -3,11 +3,15 @@
 
 static int id_push;
 
-static VALUE t_init(VALUE self, VALUE obj) {
-  VALUE factor = rb_float_new(0.97);
-  VALUE prior = rb_float_new(0);
-  rb_iv_set(self, "@factor", factor);
-  rb_iv_set(self, "@prior", prior);
+static VALUE t_init(VALUE self, VALUE args) {
+  int len = RARRAY_LEN(args);
+  if (len ==1) {
+    rb_iv_set(self, "@factor", rb_ary_entry(args, 0));
+  } else {
+    rb_iv_set(self, "@factor", rb_float_new(0.97));
+  }
+  rb_iv_set(self, "@prior", rb_float_new(0.0));
+
   return self;
 }
 
@@ -18,7 +22,6 @@ static VALUE t_left_shift(VALUE self, VALUE obj) {
   float *data = ALLOC_N(float, len);
   for (i=0;i<len;++i) {
     data[i] = NUM2DBL(ptr[i]);
-    //printf("foo %f\n", NUM2DBL(ptr[i]));
   }
   
   VALUE factor = rb_iv_get(self, "@factor");
@@ -37,7 +40,7 @@ VALUE cPreemphasis;
 void Init_preemphasis() {
   VALUE m_noyes_c = rb_define_module("NoyesC");
   cPreemphasis = rb_define_class_under(m_noyes_c, "Preemphasizer", rb_cObject);
-  rb_define_method(cPreemphasis, "initialize", t_init, 1);
+  rb_define_method(cPreemphasis, "initialize", t_init, -2);
   rb_define_method(cPreemphasis, "<<", t_left_shift, 1);
   id_push = rb_intern("push");
 }
