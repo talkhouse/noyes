29ea5cffd3a1873043c1eea4b5557b75c9f29db8
Joe Wölfel <joe@talkhouse.com>
2010-06-22 16:17:52 -0400

    Standardize the order in which fast tests are run.

--- test/ts_all_c.rb
+++ test/ts_all_c.rb
@@ -1,27 +1,8 @@
-#require 'noyes'
-require 'tu_extensions'
-require 'c_impl/noyes_c'
 require 'common'
-require 'tc_preemphasis'
-require 'tc_segment'
-require 'tc_hamming_window'
-require 'tc_power_spec'
-require 'tc_mel_filter'
-require 'tc_log_compress'
-require 'tc_dct'
-require 'tc_live_cmn'
-require 'tc_end_of_utterance'
-require 'tc_queue'
+require 'c_impl/noyes_c'
+require 'ts_fast'
 
-module NoyesC
-  class Preemphasizer
-    include NoyesFilterDSL
-  end
-  class Segmenter
-    include NoyesFilterDSL
-  end
-end
-
+#make_test 'NoyesC', 'Delta'
 make_test 'NoyesC', 'Preemphasis'
 make_test 'NoyesC', 'Segment'
 make_test 'NoyesC', 'HammingWindow'
@@ -31,4 +12,7 @@ make_test 'NoyesC', 'LogCompress'
 make_test 'NoyesC', 'DCT'
 make_test 'NoyesC', 'LiveCMN'
 make_test 'NoyesC', 'EndOfUtterance'
-make_test 'NoyesC', 'Queue'
+make_test 'NoyesC', 'PowerSpec'
+make_test 'NoyesC', 'DiscreteFourierTransform'
+#make_test 'NoyesC', 'DSL'
+#make_test 'NoyesC', 'Queue'
