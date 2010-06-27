c4f01ecfba23735d2d026923d3688ed26c6f2a54
Joe Wölfel <joe@talkhouse.com>
2010-06-26 20:33:36 -0400

    Add additional documentation to bent_cent_marker.

--- lib/ruby_impl/bent_cent_marker.rb
+++ lib/ruby_impl/bent_cent_marker.rb
@@ -1,7 +1,9 @@
 module Noyes
   # Determines whether a PCM frame is speech or not using Bent
   # Schmidt-Nielsen's algorithm.  Basically, it's an energy-based detector
-  # where the background noise level is constantly estimated.
+  # where the background noise level is constantly estimated.  You probably
+  # don't want to use this class directly.  Most of the time you'll want
+  # to use SpeechTrimmer, which uses this class.
   #
   # The pcm data should be in 100 millisecond chunks.  For example,
   # At 8000 Hz there should 80 frames of pcm.
@@ -14,6 +16,8 @@ module Noyes
       @min_signal = 0.0
       @threshold = 10.0
     end
+
+    # Take the log rms of an array of pcm values.
     def logrms pcm
       sum_of_squares = 0.0
       pcm.each {|sample| sum_of_squares += sample * sample}
@@ -21,6 +25,9 @@ module Noyes
       rms = Math.max rms, 1
       Math.log(rms) * 20
     end
+
+    # Takes a centisecond worth of pcm values and indicates whether it looks
+    # like speech.  This information is typically used by SpeechTrimmer.
     def << pcm
       is_speech = false
       current = logrms pcm
