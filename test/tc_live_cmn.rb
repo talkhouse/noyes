b9006b0f4845e2702e511deff4a2f5d0b0723c0d
Joe Wölfel <joe@talkhouse.com>
2010-08-02 13:41:10 -0400

    Add dct sums test.
    This test isn't really important.  It's just meant to reveal some strange
    behavior on windows machines.

--- test/tc_live_cmn.rb
+++ test/tc_live_cmn.rb
@@ -1,9 +1,9 @@
 module TestLiveCMN
   def test_live_cmn
-    ex_dct = IO.read('data/noyes/dct.dat').unpack 'g*'
+    flat_dct = IO.read('data/noyes/dct.dat').unpack 'g*'
     dct =[]
-    0.step ex_dct.size-13, 13 do |i|
-      dct << ex_dct[i, 13]
+    0.step flat_dct.size-13, 13 do |i|
+      dct << flat_dct[i, 13]
     end 
     ex_cmn = IO.read('data/noyes/cmn.dat').unpack 'g*'
 
@@ -11,5 +11,23 @@ module TestLiveCMN
     cmn = live_cmn << dct
     cmn_flat = cmn.flatten
     assert_m ex_cmn, cmn_flat, 5
+  end
+  def test_dct_sum
+    flat_dct = IO.read('data/noyes/dct.dat').unpack 'g*'
+    dct =[]
+    0.step flat_dct.size-13, 13 do |i|
+      dct << flat_dct[i, 13]
+    end 
+    sums = Array.new 13,0
+    dct.each do |mfc|
+      sums.size.times {|i| sums[i] += mfc[i]}
+    end
+
+
+    expected = [10242.3816947937, -257.540581060573, 143.591144894774,
+               -5.3736744876951, -13.8144978160853, -43.4587020128965, -16.08182763902,
+               2.80991833936423, -58.3629225670011, 18.788794952794, 8.30175039544702,
+               -63.9639747596812, -21.1389158053207]
+    assert_m expected, sums, 5
   end
 end
