16657ef553790a3d7c3335217cd872f55a770ecc
Joe Wölfel <joe@talkhouse.com>
2010-08-09 11:08:51 -0400

    Use assert_m in melcos test.

--- test/tc_dct.rb
+++ test/tc_dct.rb
@@ -2,16 +2,16 @@ require 'yaml'
 
 module TestDCT
   def test_dct
-    dctin = YAML.load_file 'test/yml/testdct/dctin.yml'
+    dctin = YAML.load_file 'test/yml/dct/dctin.yml'
     dct = DCT.new 13, dctin[0].size
     result = dct << dctin
-    dctexp = YAML.load_file 'test/yml/testdct/dctexp.yml'
+    dctexp = YAML.load_file 'test/yml/dct/dctexp.yml'
     assert_m dctexp, result, 6
   end
   def test_melcos
     dct = DCT.new 13, 40
     result = dct.melcos
-    expected = YAML.load_file 'test/yml/testdct/melcos-13-40.yml'
-    expected.zip(result) {|a,b| a.zip(b) {|x,y| assert_in_delta x,y,0.000001}}
+    melcosexp = YAML.load_file 'test/yml/dct/melcos-13-40.yml'
+    assert_m melcosexp, result, 6
   end 
 end
