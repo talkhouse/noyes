8b3dce5614e389e8ecebf12b3b6207a00946f08f
Joe Wölfel <joe@talkhouse.com>
2010-12-09 15:11:35 -0500

    TAUDIO is now TPCM.

--- lib/common/noyes_protocol.rb
+++ lib/common/noyes_protocol.rb
@@ -2,7 +2,7 @@ TMAGIC = '1.0 talkhouse'
 
 # The following constants are packed as 4 byte big-endian integers.
 TSTART = [0].pack('N')
-#TAUDIO = [1].pack('N')
+TPCM = [1].pack('N')
 TEND = [2].pack('N')
 TBYE = [3].pack('N')
 TCEPSTRA = [4].pack('N')
